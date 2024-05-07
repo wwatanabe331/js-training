6222c2423db7c2f81c238a47bfc2966ed0c0aafd
[wwatanabe331] <[wwatanabe331@gmail.com]>
2024-05-07 23:51:10 +0900

    lesson4

--- src/pages/Lesson04/index.js
+++ src/pages/Lesson04/index.js
@@ -4,9 +4,34 @@ import Chart from "../../components/Chart04";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  //return []; // ここを作りましょう！
+  const species = Array.from(new Set(input.map(({species}) =>species)));
+  return species.map((species) => {
+    return {
+      id: species,
+      data: input
+      .filter((item) => item.species === species)
+      .map(({spalLength: x, petalWidth: y}) => ({x, y})),
+    };
+  });
 
+  //return input.map(item => ({
+  //   id: item.species,
+  //    data: [
+  //      {x: item.sepalLength, y: item.petalWidth}
+  //    ]
+  //}));
+
+  //const iris = [
+  //  {
+  //    id: input.species,
+  //    data: [
+  //      {x: input.sepalLength, y: input.petalWidth}
+  //    ]
+  //  }
+  //];
+  //return iris;
+};
 const Lesson = () => {
   return (
     <LessonPage
