0b51220c8f884ec5e745c30131c059f8e32837af
[wwatanabe331] <[wwatanabe331@gmail.com]>
2024-05-08 15:56:53 +0900

    lesson6

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -4,7 +4,21 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  //return []; // ここを作りましょう！
+  const bins = [];
+  for(let i = 0; i < input.length; i++){
+    const item = input[i];
+    const data = {
+      color: item.gender ==="男性" ? "blue" : "red",
+      gender: item.gender,
+      bmi: item.x / ((item.y/100)**2),
+      weight: item.x,
+      height: item.y,
+    };
+   
+    bins.push(data);
+  }
+  return bins;
 };
 
 const Lesson = () => {
